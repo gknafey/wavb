0bf285cd8ab884a4b58316d55ca30b9b3335b68a
Garpken <gknafey@gmail.com>
2020-04-27 17:57:50 -0400

    created production build and edited .gitignore

---
+++ build/precache-manifest.25d924b2ef7c325bf7bfe41ed11e67ee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40e52b9bdc0aa89f6d4d14b6a8a1226",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65139f724fa49102336e",
+    "url": "/static/css/main.63398439.chunk.css"
+  },
+  {
+    "revision": "a617b875b45b705647b2",
+    "url": "/static/js/2.158a4c88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.158a4c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65139f724fa49102336e",
+    "url": "/static/js/main.304cfa86.chunk.js"
+  },
+  {
+    "revision": "868c59869960d6cad76e",
+    "url": "/static/js/runtime-main.6808c349.js"
+  },
+  {
+    "revision": "f29b35f2909b8c7441503c52abc4f230",
+    "url": "/static/media/BurgerNice.f29b35f2.JPG"
+  },
+  {
+    "revision": "3fd33eb3192d247ea2f6c86e8356152f",
+    "url": "/static/media/PoachedEggPlateB.3fd33eb3.JPG"
+  },
+  {
+    "revision": "0739ecedd668a29ebc015969d78bda6b",
+    "url": "/static/media/PoppersPlateB.0739eced.JPG"
+  },
+  {
+    "revision": "d97e28d959ca8e18ab6c6d1c2960ab9b",
+    "url": "/static/media/SliderPlateB.d97e28d9.JPG"
+  },
+  {
+    "revision": "5139a6ad54177738bf579d2503d97246",
+    "url": "/static/media/ThaiPlateB.5139a6ad.JPG"
+  },
+  {
+    "revision": "59072c97d1e1c9533031e7f642c7ff9a",
+    "url": "/static/media/WillsLogo.59072c97.jpg"
+  }
+]);
