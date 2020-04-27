403e4729207595756e5f85e1be5da564aba2f540
Garpken <gknafey@gmail.com>
2020-04-27 17:34:15 -0400

    edited some borders on the main page and made a new component to add more variety to the main page

---
+++ src/components/TitleMain2/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function TitleMain2() {
+    return (
+        <div className="main2Border2">
+        <div className="main2Border">
+        <p className="main2Title">Check out our recipes for these dishes made with our vegan burgers below!</p>
+        </div>
+        </div>
+    )
+}
+
+export default TitleMain2;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Main from "../components/MainInfo";
 import Main2Info from "../components/Main2Info";
 import Jumbotron from "../components/Jumbotron";
+import TitleMain2 from "../components/TitleMain2"
 
 
 
@@ -21,6 +22,7 @@ class Homepage extends Component {
 
           </Jumbotron>
         </div>
+        <TitleMain2 />
         <Main2Info />
       </div>
     );
